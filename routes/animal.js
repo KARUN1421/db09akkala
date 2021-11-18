070a9df246c1783faede0510aaf79547214a86f9
KARUN1421 <89479514+KARUN1421@users.noreply.github.com>
2021-11-17 19:53:02 -0600

    bonus

--- routes/animal.js
+++ routes/animal.js
@@ -2,6 +2,7 @@ var express = require('express');
 const animal_controller = require('../controllers/animal'); 
 var router = express.Router(); 
  
-router.get('/delete', animal_controller.animal_delete_Page);
+// router.get('/delete', animal_controller.animal_delete_Page);
+router.get('/detail', animal_controller.animal_view_one_Page);
 
 module.exports = router; 
