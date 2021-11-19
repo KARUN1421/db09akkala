c3d76c62b92b5559ba68ee4c893bd7b8e26e32da
KARUN1421 <89479514+KARUN1421@users.noreply.github.com>
2021-11-19 11:47:56 -0600

    added buttons

--- routes/animal.js
+++ routes/animal.js
@@ -3,6 +3,7 @@ const animal_controller = require('../controllers/animal');
 var router = express.Router(); 
  
 // router.get('/delete', animal_controller.animal_delete_Page);
-router.get('/detail', animal_controller.animal_view_one_Page);
+router.get('/', animal_controller.animal_view_all_Page);
+router.get('/update', animal_controller.animal_update_Page);
 
 module.exports = router; 
