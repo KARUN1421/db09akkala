268c5418dfc28287418469b5cb670bf563629122
KARUN1421 <89479514+KARUN1421@users.noreply.github.com>
2021-12-01 16:29:46 -0600

    authentication failure

--- routes/animal.js
+++ routes/animal.js
@@ -4,6 +4,14 @@ var router = express.Router();
  
 // router.get('/delete', animal_controller.animal_delete_Page);
 router.get('/', animal_controller.animal_view_all_Page);
-router.get('/update', animal_controller.animal_update_Page);
+
+const secured = (req, res, next) => {
+    if (req.user){
+      return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+  }
+router.get('/update', secured, animal_controller.animal_update_Page);
 
 module.exports = router; 
